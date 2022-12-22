d2daf914732aff0bacc871a529b30d326a916c3c
Primesh Heshan <primeshheshan@gmail.com>
2022-12-22 21:05:25 +0530

    imported config module

--- backend/apps/auth/src/auth.module.ts
+++ backend/apps/auth/src/auth.module.ts
@@ -7,9 +7,17 @@ import { AccessTokenStrategy } from './strategies/accessToken.strategy';
 import { RefreshTokenStrategy } from './strategies/refreshToken.strategy';
 import { UserModule } from './user/user.module';
 import { RefreshTokenCookieStrategy } from './strategies/refreshTokenCookie.strategy';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, PassportModule, JwtModule.register({})],
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({}),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AuthController],
   providers: [
     AuthService,
